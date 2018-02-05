a75ccfad3aeaa5b0693155ab67ef54fac4ff6089
Rafael Berger <Rafiberger613@gmail.com>
2018-02-05 14:57:00 -0500

    added ClassVsInstance

---
+++ ClassVsInstanceHRank/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassVsInstanceHRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int T = int.Parse(Console.In.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                int age = int.Parse(Console.In.ReadLine());
+                Person p = new Person(age);
+                p.amIOld();
+                for (int j = 0; j < 3; j++)
+                {
+                    p.yearPasses();
+                }
+                p.amIOld();
+                Console.WriteLine();
+            }
+            
+
+         
+            Console.ReadLine();
+
+            
+        }
+    }
+}
